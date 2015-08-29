b9c37263f645db3dde89a4432979b12706466ec6
Justin Cano <jcano001@ucr.edu>
2015-08-29 00:37:01 -0700

    added Grunt task to concat js files: grunt concat:dist

--- dist/mentionify.js
+++ dist/mentionify.js
@@ -1,4 +1,3 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
  * findAndReplaceDOMText v 0.4.3
  * @author James Padolsey http://james.padolsey.com
@@ -626,20 +625,11 @@
 		}
 
 	};
-findAndReplaceDOMText(document.getElementById('t'), {
-  find: /Hello/,
-  wrap: 'em'
-});
+
 	return exposed;
 
 }));
-
-},{}],2:[function(require,module,exports){
-var replace = require('findandreplacedomtext');
-
-// findAndReplaceDOMText(document.getElementById('t'), {
-//   find: /Hello/,
-//   wrap: 'em'
-// });
-
-},{"findandreplacedomtext":1}]},{},[2]);
+;findAndReplaceDOMText(document.getElementById('t'), {
+  find: /Hello/,
+  wrap: 'em'
+});
