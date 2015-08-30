98e5f5b49e7cb953da16228e1fd725a2900b9747
Justin Cano <jcano001@ucr.edu>
2015-08-30 02:26:07 -0700

    started integrating other accounts

--- dist/mentionify.js
+++ dist/mentionify.js
@@ -629,21 +629,42 @@
 	return exposed;
 
 }));
-;findAndReplaceDOMText(document.getElementById('example'), {
-  find: /(\@)(\w+)/,
-  replace: function(portion, match) {
-    var whole = match[0], mention = match[1], username = match[2],
-        a = document.createElement('a'),
-        href = '//twitter.com/' + username,
-        text = document.createTextNode(whole);
+;var default_options = {
+        account: 'twitter'
+    },
+    user_options = null;
 
-    a.setAttribute('href', href);
-    a.appendChild(text);
+function Mentionify() {
 
-    return a;
-  },
-  forceContext: function(el) {
-    // Using https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
-    return el.matches('p');
-  }
-});
+    this.run = function(options) {
+        user_options = typeof options !== 'undefined' ?  options : default_options;
+        for (var option in default_options) {
+            user_options[option] = user_options[option] || default_options[option];
+        }
+
+        findAndReplaceDOMText(document.getElementById('example'), {
+          account: 'test',
+          find: /(\@)(\w+)/,
+          replace: function(portion, match) {
+                var whole = match[0], mention = match[1], username = match[2],
+                    a = document.createElement('a'),
+                    href = '//' + user_options.account + '.com/' + username,
+                    text = document.createTextNode(whole);
+
+                    console.log(href);
+                a.setAttribute('href', href);
+                a.appendChild(text);
+
+                return a;
+            },
+            forceContext: function(el) {
+                // Using https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
+                return el.matches('p');
+            }
+        });
+    };
+
+}
+
+var mentionify = new Mentionify();
+mentionify.run({account:'github'});
