cb9f69e4b9579843e13360f458f5f8bc2d759c4b
Justin Cano <jcano001@ucr.edu>
2015-08-30 01:11:50 -0700

    mentionified😁

--- src/mentionify.js
+++ src/mentionify.js
@@ -1,4 +1,18 @@
 findAndReplaceDOMText(document.getElementById('example'), {
-  find: /\@canokiid/,
-  wrap: 'strong'
+  find: /(\@)(\w+)/,
+  replace: function(portion, match) {
+    var whole = match[0], mention = match[1], username = match[2],
+        a = document.createElement('a'),
+        href = '//twitter.com/' + username,
+        text = document.createTextNode(whole);
+
+    a.setAttribute('href', href);
+    a.appendChild(text);
+
+    return a;
+  },
+  forceContext: function(el) {
+    // Using https://developer.mozilla.org/en-US/docs/Web/API/Element/matches
+    return el.matches('p');
+  }
 });
