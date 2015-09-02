7d33aa575b3be952a5f06778a5d85146107d4d21
Justin Cano <jcano001@ucr.edu>
2015-09-01 23:57:19 -0700

    added linkedin and reddit to unit tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,20 +1,9 @@
-accounts = {
-    "linkedin": "linkedin.com/in/",
-    "reddit": "reddit.com/r/"
-};
-
-function buildURL(account) {
-    if (!(account in accounts))
-        return "://" + account + ".com/";
-    return accounts[account];
-}
-
 function testMentionify(account) {
     QUnit.test("mentionify." + account, function( assert ) {
         var links = document.getElementById(account).getElementsByClassName("mentionified");
         for (var i = 0; i < links.length; i++) {
-            href = links[i].href.slice(links[i].href.indexOf("://"));
-            url = buildURL(account) + links[i].innerHTML.slice(1);
+            href = links[i].href.slice(links[i].href.indexOf("//"));
+            url = "//" + getAccountUrl(account) + links[i].innerHTML.slice(1);
             assert.equal(href, url);
         }
     });
@@ -32,5 +21,7 @@ testMentionify("github");
 testMentionify("facebook");
 testMentionify("portfolium");
 testMentionify("soundcloud");
+testMentionify("linkedin");
+testMentionify("reddit");
 
 testEmail();
