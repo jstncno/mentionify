dbd14588bde4a2a2b9f868032a90e4044b755542
efexen <fxnstyling@gmail.com>
2015-12-01 12:25:46 +0000

    Fixed the test order to correctly concat the files prior to qunit

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,8 +52,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
-  grunt.registerTask('test', ['jshint', 'qunit']);
-
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+  grunt.registerTask('test', ['jshint', 'concat', 'qunit']);
+  grunt.registerTask('default', ['test', 'uglify']);
 
 };
