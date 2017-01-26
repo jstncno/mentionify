2366df320351803487aacdfb40101d494d06f3df
Justin Cano <jcano001@ucr.edu>
2017-01-25 22:05:21 -0800

    New build and test workflow
    closes #12
    
    initial webpack stuffs

--- src/mentionify.js
+++ src/mentionify.js
@@ -1,3 +1,5 @@
+import findAndReplaceDOMText from 'findandreplacedomtext';
+
 var defaultOptions = {
         "elementId": "container",
         "account": "twitter",
@@ -52,3 +54,16 @@ function Mentionify() {
     };
 
 }
+
+
+var mentionify = new Mentionify();
+mentionify.run({elementId: 'twitter'});
+mentionify.run({elementId: 'github', account:'github'});
+mentionify.run({elementId: 'facebook', account:'facebook'});
+mentionify.run({elementId: 'portfolium', account:'portfolium'});
+mentionify.run({elementId: 'soundcloud', account:'soundcloud'});
+mentionify.run({elementId: 'linkedin', account:'linkedin'});
+mentionify.run({elementId: 'reddit', account:'reddit'});
+mentionify.run({elementId: 'auto', account:'auto'});
+mentionify.run({elementId: 'specified-class-name', account:'facebook', className:'some-class-name'})
+mentionify.run({elementId: 'email'});
