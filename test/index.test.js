7ba79677dbafd525a7591c97801da8a0b41d960b
Justin Cano <jcano001@ucr.edu>
2017-01-28 19:41:46 -0800

    added the rest of the tests

--- test/index.test.js
+++ test/index.test.js
@@ -19,10 +19,10 @@ function getMentionIndex(string) {
     }
 }
 
-describe('mentionify', function( assert ) {
+describe('mentionify', function() {
     sites.map( site =>
-        describe(site, function( assert ) {
-            it('should match with @\'s in strings', function() {
+        describe(site, function() {
+            it('should render @\'s to links', function() {
                 var links = document.getElementById(site).getElementsByClassName('mentionified');
                 for (var i = 0; i < links.length; i++) {
                     href = links[i].href.slice(links[i].href.indexOf('//'));
@@ -32,4 +32,38 @@ describe('mentionify', function( assert ) {
             });
         })
     );
+
+    describe('email', function() {
+        it('should not render email link', function() {
+            var links = document.getElementById('email').getElementsByClassName('mentionified');
+            assert.equal(links.length, 0);
+        });
+    });
+
+    describe('auto', function( assert ) {
+        it('should automatically parse and render site link', function() {
+            var links = document.getElementById('auto').getElementsByClassName('mentionified');
+            var sites = [
+                { type: 'twitter', username: 'jcvno' },
+                { type: 'reddit', username: 'canoj' }
+            ];
+
+            for (var i = 0; i < links.length; i++) {
+                href = links[i].href.slice(links[i].href.indexOf('//'));
+                url = '//' + getSiteUri(sites[i].type) + sites[i].username;
+                assert.equal(href, url);
+            }
+        });
+    });
+
+    describe('specifiedClassName', function( assert ) {
+        it('should created the specified class name', function() {
+            var links = document.getElementById('specified-class-name').getElementsByClassName('some-class-name');
+            for (var i = 0; i < links.length; i++) {
+                href = links[i].href.slice(links[i].href.indexOf('//'));
+                url = '//' + getAccountUri('facebook') + links[i].innerHTML.slice(getMentionIndex(links[i].innerHTML));
+                assert.equal(href, url);
+            }
+        });
+    });
 });
--- test/regexes.test.js
+++ test/regexes.test.js
@@ -4,10 +4,10 @@ import assert from 'assert';
 describe('regex', function() {
   describe('default regexes', function() {
     it('should match with @\'s in strings', function() {
-      assert.ok("@jcvno".match(regexes["default"]));
-      assert.ok("@github and @githubstatus".match(regexes["default"]));
-      assert.ok("I am mentioning @jcvno: hello!".match(regexes["default"]));
-      assert.ok("@_underscore_".match(regexes["default"]));
+      assert.ok('@jcvno'.match(regexes['default']));
+      assert.ok('@github and @githubstatus'.match(regexes['default']));
+      assert.ok('I am mentioning @jcvno: hello!'.match(regexes['default']));
+      assert.ok('@_underscore_'.match(regexes['default']));
     });
   });
 });
