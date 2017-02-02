e628ce4dee2d1f8d789dcc817a28abfb08078a61
Justin Cano <earthican@users.noreply.github.com>
2017-02-01 22:25:59 -0800

    Build as node module (#14)
    * New build and test workflow
    
    closes #12
    
    initial webpack stuffs
    
    * removed lib/ from .gitignore
    
    * added simple mocha test case
    
    * added some build stuffs
    
    * added lib/, removed dist/
    
    * adding jsdom for testing
    
    * use jsdom to test
    
    * let mocha discover test cases
    
    * added the rest of the tests
    
    * updated travis config
    
    * passes stuff
    
    * update test scripts
    
    * bin only needed in test
    
    * added babel-cli to dev deps
    
    * code cleanup
    
    * no need to minify module
    
    * added integration (bundle) testing
    
    * no need for class, just include in module
    
    * updated README
    
    * fix bug in test
    
    * don't need uglify
    
    * removed example.html
    
    * bump version

--- src/mentionify.js
+++ src/mentionify.js
@@ -1,45 +1,24 @@
-var defaultOptions = {
-        "elementId": "container",
-        "account": "twitter",
-        "className": "mentionified"
-    },
-    userOptions = null;
+import findAndReplaceDOMText from 'findandreplacedomtext';
+import { defaultOptions, regexes , getSiteUri } from './utils';
 
-var accounts = {
-    "linkedin": "linkedin.com/in/",
-    "reddit": "reddit.com/u/"
-};
-
-var regexes = {
-    "default": /\B(\@)([a-zA-Z0-9-_]+)\b/g,
-    "linkedin": /\B(\/in\/)([a-zA-Z0-9-_]+)\b/g,
-    "reddit": /\B(\/u\/)([a-zA-Z0-9-_]+)\b/g,
-    "auto": /\B(\@)([a-zA-Z0-9-_]+)\(([a-zA-Z0-9-_]+)\)/g
-};
-
-function getAccountUri(account) {
-    return accounts[account] ? accounts[account] : account + ".com/";
+function getRegex(site) {
+    return regexes[site] ? regexes[site] : regexes["default"];
 }
 
-function getRegex(account) {
-    return regexes[account] ? regexes[account] : regexes["default"];
-}
-
-function Mentionify() {
-
-    this.run = function(options) {
-        userOptions = typeof options !== "undefined" ?  options : defaultOptions;
+module.exports = {
+    run: function(options) {
+        var userOptions = typeof options !== "undefined" ?  options : defaultOptions;
         for (var option in defaultOptions) {
             userOptions[option] = userOptions[option] || defaultOptions[option];
         }
 
         findAndReplaceDOMText(document.getElementById(userOptions.elementId), {
-          find: getRegex(userOptions.account),
+          find: getRegex(userOptions.site),
           replace: function(portion, match) {
                 var whole = match[0], mention = match[1], username = match[2],
-                    account = match[3] || userOptions.account,
+                    site = match[3] || userOptions.site,
                     a = document.createElement("a"),
-                    href = "//" + getAccountUri(account) + username,
+                    href = "//" + getSiteUri(site) + username,
                     text = document.createTextNode(whole);
 
                 a.setAttribute("href", href);
@@ -49,6 +28,9 @@ function Mentionify() {
                 return a;
             }
         });
-    };
+    },
 
+    regexes: regexes,
+
+    getSiteUri: getSiteUri
 }
---
+++ test/it/bundle.test.js
@@ -0,0 +1,12 @@
+import Mentionify from '../../lib/mentionify';
+
+Mentionify.run({elementId: 'twitter'});
+Mentionify.run({elementId: 'github', site:'github'});
+Mentionify.run({elementId: 'facebook', site:'facebook'});
+Mentionify.run({elementId: 'portfolium', site:'portfolium'});
+Mentionify.run({elementId: 'soundcloud', site:'soundcloud'});
+Mentionify.run({elementId: 'linkedin', site:'linkedin'});
+Mentionify.run({elementId: 'reddit', site:'reddit'});
+Mentionify.run({elementId: 'auto', site:'auto'});
+Mentionify.run({elementId: 'specified-class-name', site:'facebook', className:'some-class-name'})
+Mentionify.run({elementId: 'email'});
